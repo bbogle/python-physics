c493466205e6d100a5bca10150e910fbd2c48b46
Syed Zahir Bokhari <syedzahirbokhari@gmail.com>
2013-01-25 23:41:48 -0500

    Fix history

---
+++ GalaxyParts.py
@@ -0,0 +1,136 @@
+from __future__ import division
+from math import *
+from Vector import *
+import pygame
+import Physics as Phy
+
+class VectorDraw(Vector):
+    def __init__(self, vec, x=0, y=0):
+        Vector.__init__(self, vec.x, vec.y)
+        self.px = x
+        self.py = y
+
+    def draw(self, surf, color=pygame.Color(0,0,0)):
+        pygame.draw.line(surf, color, (self.px,self.py), 
+                         (self.px + self.x, self.py + self.y))
+        pygame.draw.circle(surf, color, (self.px + self.x, self.py + self.y), 2)
+
+
+class Piece:
+    def __init__(self, pos=None, mass=10, vel=None, acc=None):
+        self.pos = pos or Vector()
+        self.vel = vel or Vector()
+        self.acc = acc or Vector()
+        self.mass = mass
+        
+    def move(self, t=Phy.CONST_T):
+        oldvel = self.vel
+        self.vel = self.vel + self.acc * t
+        self.pos = self.pos + (self.vel + oldvel) * 0.5 * t
+
+    def draw(self, win):
+        raise NoImplementationError("Draw function not implemented")
+         
+
+class Ball(Piece):
+    def __init__(self, pos, rad=10, vel=None, acc=None, cr=1, fr=1):
+        Piece.__init__(self, pos, 1, vel, acc)
+        self.rad = rad
+        self.color=pygame.Color(255, 0, 0)
+        self.cr = cr
+        self.fr = fr
+
+    def _checkWallOverlap(self, walls):
+        for b in walls:
+            n = b.getNorm()             #Normal to the line away from bouncing side
+            p = b.getMid()-self.pos     #vector from ball to center of line
+            dist = abs(p.dot(n))        #distance from center of ball to the line
+            l = p - n*p.dot(n)          #perp = p - proj on normal
+            if dist < self.rad and l.mag < ((b.p2-b.p1).mag/2):
+                if self.vel.mag != 0:
+                    t = abs((self.rad-dist)/n.dot(self.vel))
+                    self.pos = self.pos - self.vel*t
+                    proj  = n * n.dot(self.vel)
+                    perp = self.vel - proj
+                    self.vel = perp*self.fr - proj*self.cr
+                    self.pos = self.pos + self.vel * (1-t)
+
+    def _checkFutureWall(self, walls):
+        for b in walls:
+            (x1, y1) = self.pos.rect()
+            (u1, v1) = self.vel.rect()
+            (x2, y2) = b.getMid().rect()
+            (u2, v2) = (b.p1 - b.getMid()).rect()
+            det = u2*v1-u1*v2
+            if det != 0:
+                (dx, dy) = (x2-x1, y2-y1)
+                t = (u2*dy-v2*dx)/det
+                s = (u1*dy-v1*dx)/det
+                if t <= 1 and t >= 0 and abs(s) <= 1:
+                    t = t - self.rad/self.vel.dot(b.getNorm())
+                    print(t)
+                    n = b.getNorm()
+                    self.pos = self.pos + self.vel*t
+                    proj  = n * n.dot(self.vel)
+                    perp = self.vel - proj
+                    self.vel = perp*self.fr - proj*self.cr
+                    self.pos = self.pos + self.vel * (1-t)
+
+    def _overlapOtherBall(self, other):
+        dp = self.pos - other.pos
+        dvel = other.vel - self.vel
+        r = self.rad + other.rad
+        (dx, dy) = dp.rect()
+        (du, dv) = dvel.rect()
+        (a, b, c) = (du**2+dv**2, 2*(dx*du+dy*dv), dx**2+dy**2-r**2)
+        descriminant = b**2 - 4*a*c
+        if (descriminant >= 0 and a != 0):
+            t = (-b+sqrt(descriminant))/(2*a)
+            t2 = (-b-sqrt(descriminant))/(2*a)
+            if (dp.mag < r):
+                self.pos = self.pos - self.vel*t
+                other.pos = other.pos - other.vel*t
+            elif (t >= 0 and t <= 1):
+                if t2 > 0: goodt = t2
+                else: goodt = t
+                self.pos = self.pos + self.vel*t
+                other.pos = other.pos + other.vel*t
+
+    def collide(self, other):
+        self._overlapOtherBall(other)
+
+    def bounderyCheck(self, walls):
+        self._checkWallOverlap(walls)
+        self._checkFutureWall(walls)
+
+    def draw(self, surf):
+        pygame.draw.circle(surf, self.color, self.pos.rect(), self.rad)
+        vd = VectorDraw(self.vel, self.pos.x, self.pos.y)
+        vd.draw(surf)
+
+class Wall:
+    def __init__(self, p1, p2):
+        self.p1 = p1
+        self.p2 = p2
+        vec = p2-p1
+        vec.setRect(vec.x, -vec.y)
+        self.angle = vec.theta
+
+    def getNorm(self):
+        v = self.p2-self.p1
+        v.setMag(1)
+        v.setRect(-v.y, v.x)
+        return v
+
+    def getMid(self):
+        return (self.p1+self.p2)*0.5
+        
+
+    def draw(self, surf, color=pygame.Color(0, 0, 0)):
+        pygame.draw.line(surf, color, self.p1.rect(), self.p2.rect())
+        mid = (self.p1 + self.p2) * 0.5
+        ac = 10
+        area = pygame.Rect(mid.x-ac, mid.y-ac, 2*ac, 2*ac)
+        pygame.draw.arc(surf, color, area, self.angle-pi, self.angle, 1)
+        vd = VectorDraw(self.getNorm()*20, mid.x, mid.y)
+        vd.draw(surf)
---
+++ main.py
@@ -0,0 +1,65 @@
+from __future__ import division
+import pygame
+import sys
+from GalaxyParts import *
+from pygame.locals import *
+from random import random
+from Vector import *
+import Physics as Phy
+
+WIDTH = 640
+HEIGHT = 480
+
+pygame.init()
+
+fpsClock = pygame.time.Clock()
+
+white = pygame.Color(255,255,255)
+black = pygame.Color(0,0,0)
+
+win = pygame.display.set_mode((WIDTH, HEIGHT))
+
+gameobjects = []
+pad = 40
+ov = 0
+walls = [Wall(Vector(pad-ov+400, HEIGHT-pad), Vector(WIDTH-pad+ov, HEIGHT-pad)),
+         Wall(Vector(WIDTH-pad, HEIGHT-pad+ov), Vector(WIDTH-pad, pad-ov)),
+         Wall(Vector(WIDTH-pad+ov, pad), Vector(pad-ov, pad)),
+         Wall(Vector(pad, pad-ov), Vector(pad, pad+200+ov)),
+         Wall(Vector(pad-ov, pad+200), Vector(pad+400, HEIGHT-pad+ov)),
+         Wall(Vector(200, 200), Vector(WIDTH-pad, 200))]
+
+while True:
+    win.fill(white)
+
+    for i in range(0, len(gameobjects)):
+        obj = gameobjects[i]
+        (x,y) = obj.pos.rect()
+
+        for j in range(i+1, len(gameobjects)):
+            other = gameobjects[j]
+            obj.collide(other)
+
+        obj.move()
+        obj.bounderyCheck(walls)
+        obj.draw(win)
+
+    for obj in walls:
+        obj.draw(win)
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+        elif event.type == MOUSEBUTTONDOWN:
+            (x,y) = event.pos
+            tmp = Ball(Vector(x,y), 10)
+            tmp.vel.setRect(20, 10)#random()*20-10, random()*20-10)
+            tmp.acc.setRect(0, 1)
+            tmp.cr=0.98
+            tmp.fr=1
+            gameobjects.append(tmp)
+
+    pygame.display.update()
+    fpsClock.tick(45)
+
